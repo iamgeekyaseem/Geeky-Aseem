1f04421464d765f42525a975c5859793dc7b06a5
Aseem Gupta <iamgeekyaseem@gmail.com>
2025-02-27 02:52:05 +0530

    added home page

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -12,7 +12,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
       <button
         onClick={() => setMenuOpen(false)}
         className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer"
-        aria-lable="Close Menu"
+        aria-label="Close Menu"
       >
         &times;
       </button>
